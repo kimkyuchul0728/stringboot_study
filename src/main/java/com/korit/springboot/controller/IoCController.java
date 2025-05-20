d919920fc4405e86911e92129b4900e56b28cb40
Kyuchul <kyuchul0728g@gmail.com>
2025-05-20 17:09:25 +0900

    spring boot

---
+++ src/main/java/com/korit/springboot/controller/IoCController.java
@@ -0,0 +1,29 @@
+package com.korit.springboot.controller;
+
+import com.korit.springboot.ioc.삼성배터리;
+import com.korit.springboot.ioc.스마트폰;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@AllArgsConstructor
+public class IoCController {
+
+    private 스마트폰 s;
+
+    @GetMapping("/ioc")
+    public String get() {
+        s.전원켜기();
+        return null;
+    }
+
+    @GetMapping("/ioc2")
+    public String get2() {
+        s.전원켜기();
+        return null;
+    }
+
+}
